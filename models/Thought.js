afaded88002a995abe82931c3cd1d38f91f114b4
keurid <kevindesantiago99@gmail.com>
2023-09-11 22:12:57 -0700

    finished a thought.js

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toLocaleString(),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [
+    {
+      reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => new Date(timestamp).toLocaleString(),
+      },
+    },
+  ],
+});
